b23ee1e9b7f09e31c6d30a9e1dda39bf7173ffc0
Trevor Durán <trevor.m.duran@gmail.com>
2026-04-30 21:26:24 -0500

    :recycle: refactor: adjust to new naming from Generics to Env

--- src/routes/riddles.ts
+++ src/routes/riddles.ts
@@ -1,8 +1,8 @@
 import { Hono } from "hono";
-import type { Generics } from "../entry";
+import type { Env } from "../entry";
 
 // Must use chaining in order for Hono RPC to work
-const riddlesRouter = new Hono<Generics>().get("/", (c) => {
+const riddlesRouter = new Hono<Env>().get("/", (c) => {
   return c.json({ error: "Not implemented" }, 501);
 });
 
