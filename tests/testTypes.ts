81486c7d584b9ff52e61492bd7fd15a503dc2e2a
Trevor Durán <trevclev0@gmail.com>
2026-05-01 16:15:44 +0000

    :recycle: refactor: migrate the defaultNullish props to their own types file for tests

---
+++ tests/testTypes.ts
@@ -0,0 +1,16 @@
+const defaultNullishProgramProps = {
+  selectedAt: null,
+  completedAt: null,
+};
+
+const defaultNullishGateProps = {
+  solvedAt: null,
+  attemptCount: 0,
+  guidanceEnabled: false,
+  guidancePrompt: null,
+  guidanceThreshold: 0,
+  acceptanceThreshold: 0.0,
+  programId: null,
+};
+
+export { defaultNullishGateProps, defaultNullishProgramProps };
