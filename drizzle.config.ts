0814d9c42806262b7f94455d4cb0ca5674a7d075
Trevor Durán <trevor.m.duran@gmail.com>
2026-05-02 06:33:13 -0500

    :hammer: chore: add environment variable for Drizzle database URL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,14 @@
+import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
+
+const { DRIZZLE_DATABASE_URL } = process.env;
+if (!DRIZZLE_DATABASE_URL) throw new Error("Missing DRIZZLE_DATABASE_URL");
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./migrations",
   dialect: "sqlite",
   dbCredentials: {
-    url: ".wrangler/state/v3/d1/miniflare-D1DatabaseObject/68b35e1e2f51a6a1a48d7b8210bd41a01c4e1d085f25a78a4c0d51aea32ea598.sqlite",
+    url: DRIZZLE_DATABASE_URL,
   },
 });
