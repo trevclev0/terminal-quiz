4aa4ad3cf86754bf0428af55ce638067b5a8327f
Trevor Durán <trevor.m.duran@gmail.com>
2026-05-02 06:39:45 -0500

    :recycle: refactor: create a client to provide to the Drizzle object

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
+import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 
 const DB_FILE_NAME = process.env.DB_FILE_NAME;
 if (!DB_FILE_NAME) throw new Error("Cannot read database file");
 
-const db = drizzle(DB_FILE_NAME);
+const client = createClient({ url: DB_FILE_NAME });
+const db = drizzle(client);
 
 export default db;
