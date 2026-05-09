eda40f3de9e39c4502e36711cf8e5a1fb667139c
Trevor Durán <trevor.m.duran@gmail.com>
2026-05-09 08:16:14 -0500

    :recycle: refactor: migrate QueryClientProvider to main.tsx

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -1,16 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import ErrorBoundary from "@components/ErrorBoundary";
 import App from "./App";
 
+const queryClient = new QueryClient();
+
 const rootElement = document.getElementById("app-root") as HTMLElement;
 
 const root = createRoot(rootElement);
 root.render(
   <StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <QueryClientProvider client={queryClient}>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </QueryClientProvider>
   </StrictMode>,
 );
