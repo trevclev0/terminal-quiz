f33bb6c8dd7b18de1ef53cada193a5517b80ba57
Trevor Durán <trevor.m.duran@gmail.com>
2026-05-09 15:18:11 -0500

    :hammer: chore: configure Vite to utilize the path aliases defined in tsconfig files

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,18 @@
-import { fileURLToPath } from "node:url";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react(), !process.env.VITEST && cloudflare()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@hooks": fileURLToPath(
-        new URL("./src/react-app/hooks", import.meta.url),
-      ),
-      "@components": fileURLToPath(
-        new URL("./src/react-app/components", import.meta.url),
-      ),
-      "@contexts": fileURLToPath(
-        new URL("./src/react-app/contexts", import.meta.url),
-      ),
-      "@utils": fileURLToPath(
-        new URL("./src/react-app/utils", import.meta.url),
-      ),
-      "@api": fileURLToPath(new URL("./src/react-app/api", import.meta.url)),
-      "@shared": fileURLToPath(new URL("./src/shared", import.meta.url)),
-      "@worker": fileURLToPath(new URL("./src/worker", import.meta.url)),
-    },
-  },
   build: {
     minify: "oxc",
+  },
+  resolve: {
+    tsconfigPaths: true,
   },
   test: {
     environment: "happy-dom",
     setupFiles: "src/react-app/test-utils/setupTests.ts",
-    alias: {
-      "@test-utils": fileURLToPath(
-        new URL("./src/react-app/test-utils", import.meta.url),
-      ),
-    },
     clearMocks: true,
     restoreMocks: true,
     coverage: {
