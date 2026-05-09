dd68f0b92f2428dec556b00b0ac5d0e7de3915c8
Trevor Durán <trevor.m.duran@gmail.com>
2026-05-09 10:41:29 -0500

    :recycle: refactor: use persistent query client provider to enable storage syncing

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -1,21 +1,23 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { indexedDBMessagePackPersister, queryClient } from "@api/queryClient";
 import ErrorBoundary from "@components/ErrorBoundary";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import App from "./App";
-
-const queryClient = new QueryClient();
 
 const rootElement = document.getElementById("app-root") as HTMLElement;
 
 const root = createRoot(rootElement);
 root.render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister: indexedDBMessagePackPersister }}
+    >
       <ErrorBoundary>
         <App />
       </ErrorBoundary>
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   </StrictMode>,
 );
