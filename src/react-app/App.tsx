32faee17c699a3f59b34b3d53ee26cdb960d4cda
Trevor Durán <trevor.m.duran@gmail.com>
2026-05-22 18:28:49 -0500

    :recycle: refactor: Remove context provider

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,8 +1,6 @@
 import ProgramSelector from "@components/ProgramSelector";
 import ProgramWithGates from "@components/ProgramWithGates";
-import { ProgramDataContext } from "@contexts/ProgramDataContext";
 import usePrograms from "@hooks/usePrograms";
-import { useMemo } from "react";
 
 function App() {
   const {
@@ -16,16 +14,6 @@ function App() {
     clearActiveProgram,
   } = usePrograms();
 
-  const contextValue = useMemo(
-    () => ({
-      programs,
-      activeProgram,
-      selectProgram,
-      updateProgram,
-    }),
-    [programs, activeProgram, selectProgram, updateProgram],
-  );
-
   if (isLoading) {
     return <h2 className="loading-screen">Loading...</h2>;
   }
@@ -36,18 +24,16 @@ function App() {
 
   return (
     <div className="app">
-      <ProgramDataContext.Provider value={contextValue}>
-        {activeProgram ? (
-          <ProgramWithGates
-            program={activeProgram}
-            resetProgram={resetProgram}
-            clearActiveProgram={clearActiveProgram}
-            updateProgram={updateProgram}
-          />
-        ) : (
-          <ProgramSelector programs={programs} selectProgram={selectProgram} />
-        )}
-      </ProgramDataContext.Provider>
+      {activeProgram ? (
+        <ProgramWithGates
+          program={activeProgram}
+          resetProgram={resetProgram}
+          clearActiveProgram={clearActiveProgram}
+          updateProgram={updateProgram}
+        />
+      ) : (
+        <ProgramSelector programs={programs} selectProgram={selectProgram} />
+      )}
     </div>
   );
 }
