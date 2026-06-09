f1dfed84f99bcba67f6353e0a0d04a1581267425
Trevor Durán <trevor.m.duran@gmail.com>
2026-06-09 16:34:08 -0500

    :recycle: refactor: rename the rendered component for /select

--- src/react-app/routes/select.tsx
+++ src/react-app/routes/select.tsx
@@ -1,9 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/select")({
-  component: RouteComponent,
+  component: SelectComponent,
 });
 
-function RouteComponent() {
+function SelectComponent() {
   return <div>Hello "/selector"!</div>;
 }
