20190569a4603937d3f55e42feead6a4dd057bdb
Trevor Durán <trevor.m.duran@gmail.com>
2026-06-17 22:57:56 -0500

    :recycle: refactor: Remove unnecessary function call from programsQueryOptions

--- src/react-app/routes/select.tsx
+++ src/react-app/routes/select.tsx
@@ -4,7 +4,7 @@ import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/select")({
   loader: async ({ context: { queryClient } }) => {
-    await queryClient.ensureQueryData(programsQueryOptions());
+    await queryClient.ensureQueryData(programsQueryOptions);
 
     return {};
   },
