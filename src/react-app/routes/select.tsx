0ea5f41815faa1a8346754c3a1a22fc2120ad5f7
Trevor Durán <trevor.m.duran@gmail.com>
2026-06-18 21:07:19 -0500

    :recycle: refactor: Add search validation to select route

--- src/react-app/routes/select.tsx
+++ src/react-app/routes/select.tsx
@@ -2,10 +2,18 @@ import { programsQueryOptions } from "@api/queries/useProgramsQuery";
 import ProgramSelector from "@components/ProgramSelector";
 import { createFileRoute } from "@tanstack/react-router";
 
+type SelectSearch = {
+  programId?: string;
+};
+
 export const Route = createFileRoute("/select")({
+  validateSearch: (search: Record<string, unknown>): SelectSearch => {
+    return {
+      programId: search.programId as string | undefined,
+    };
+  },
   loader: async ({ context: { queryClient } }) => {
     await queryClient.ensureQueryData(programsQueryOptions);
-
     return {};
   },
   component: ProgramSelector,
