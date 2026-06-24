d7a1914c71a54a6743a0705e5ccac4598b3adc92
Trevor Durán <trevor.m.duran@gmail.com>
2026-06-23 21:33:42 -0500

    :recycle: refactor: Add programs query to program route loader

--- src/react-app/routes/programs/$programId.tsx
+++ src/react-app/routes/programs/$programId.tsx
@@ -1,12 +1,16 @@
 import { programProgressionQueryOptions } from "@api/queries/useProgramProgressionQuery";
+import { programsQueryOptions } from "@api/queries/useProgramsQuery";
 import { createFileRoute } from "@tanstack/react-router";
 import ProgramPlay from "../../components/ProgramPlay";
 
 export const Route = createFileRoute("/programs/$programId")({
   loader: async ({ context: { queryClient }, params }) => {
-    await queryClient.ensureQueryData(
-      programProgressionQueryOptions(params.programId),
-    );
+    await Promise.all([
+      queryClient.ensureQueryData(
+        programProgressionQueryOptions(params.programId),
+      ),
+      queryClient.ensureQueryData(programsQueryOptions),
+    ]);
     return { programId: params.programId };
   },
   component: ProgramPlay,
