f7009d5ca12b95903899dc0a0ecd0c0b26687199
Trevor Durán <trevor.m.duran@gmail.com>
2026-07-07 22:24:20 -0500

    🐛 fix(graphql): cast context to any in GraphQL route

--- src/worker/routes/graphql.ts
+++ src/worker/routes/graphql.ts
@@ -72,7 +72,7 @@ const graphQlRouter = new Hono<AppVariables>().use("*", async (c, next) => {
   return graphqlServer({
     schema: cachedSchema,
     graphiql: !isProduction,
-  })(c, next);
+  })(c as any, next);
 });
 
 export default graphQlRouter;
